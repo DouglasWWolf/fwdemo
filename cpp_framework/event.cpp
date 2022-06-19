7d8375d1ae750e6fb2074f2d15f9e45b8b634a59
DouglasWWolf <douglaswolf@yahoo.com>
2022-06-18 18:18:31 -0700

    See History.h for changes

--- cpp_framework/event.cpp
+++ cpp_framework/event.cpp
@@ -1,19 +1,18 @@
 //============================================================================
-// Event.cpp - Implements an event manager
+// event.cpp - Implements an event manager
 //============================================================================
 #include "event.h"
 #include <unistd.h>
 #include <sys/eventfd.h>
 #include <sys/select.h>
 
-size_t dummy_size_t;
 
 //============================================================================
 // Constructor - Creates the event object in the untriggered state
 //============================================================================
 CEvent::CEvent()
 {
-	m_fd = eventfd(0, 0);
+    m_fd = eventfd(0, 0);
 }
 //============================================================================
 
@@ -23,31 +22,31 @@ CEvent::CEvent()
 //============================================================================
 CEvent::~CEvent()
 {
-	if (m_fd > 0) close(m_fd);
-	m_fd = -1;
+    if (m_fd > 0) close(m_fd);
+    m_fd = -1;
 }
 //============================================================================
 
 
 //============================================================================
-// IsTriggered() - Returns 'true' if the event is currently triggered
+// is_triggered() - Returns 'true' if the event is currently triggered
 //============================================================================
-bool CEvent::IsTriggered()
+bool CEvent::is_triggered()
 {
-	fd_set rfds;
+    fd_set rfds;
 
-	// We're effectively going to poll
-	timeval timeout = {0,0};
+    // We're effectively going to poll
+    timeval timeout = {0,0};
 
-	// We're only polling just the one specified file-descriptor
-	FD_ZERO(&rfds);
-	FD_SET(m_fd, &rfds);
+    // We're only polling just the one specified file-descriptor
+    FD_ZERO(&rfds);
+    FD_SET(m_fd, &rfds);
 
-	// Is there data on this descriptor?
-	int count = select(m_fd+1, &rfds, NULL, NULL, &timeout);
+    // Is there data on this descriptor?
+    int count = select(m_fd+1, &rfds, NULL, NULL, &timeout);
 
-	// Tell the caller whether there is an event value ready for reading
-	return (count > 0);
+    // Tell the caller whether there is an event value ready for reading
+    return (count > 0);
 }
 //============================================================================
 
@@ -56,58 +55,64 @@ bool CEvent::IsTriggered()
 //============================================================================
 // Reset() - Resets this object to the "untriggered" state
 //============================================================================
-void CEvent::Reset()
+void CEvent::reset()
 {
-	char value[8];
+    char value[8];
 
-	// If the event is in the triggered state, clear it
-	if (IsTriggered()) dummy_size_t = read(m_fd, &value, sizeof(value));
+    // If the event is in the triggered state, clear it
+    if (is_triggered()) read(m_fd, &value, sizeof value);
 }
 //============================================================================
 
 
 //============================================================================
-// Set() - Triggers the event with the specified value
+// set() - Triggers the event with the specified value
 //============================================================================
-void CEvent::Set(unsigned long long value)
+void CEvent::set(uint64_t value)
 {
-	dummy_size_t = write(m_fd, &value, sizeof(value));
+    write(m_fd, &value, sizeof(value));
 }
 //============================================================================
 
 
 //============================================================================
-// Get() - Waits the specified number of milliseconds for the event to
-//         become triggered.
+// wait() - Waits the specified number of milliseconds for the event to
+//          become triggered.
 //
-// Returns:  The event value, or 0 if the event is untriggered
+// Passed:  milliseconds = number of milliseconds to wait.  0 = Forever
+//
+// Returns: The event value, or 0 if the event is untriggered
 //============================================================================
-uint64_t CEvent::Get(int milliseconds)
+uint64_t CEvent::wait(uint32_t milliseconds)
 {
-	uint64_t	event_value;
-	fd_set		rfds;
+    uint64_t    event_value;
+    fd_set      rfds;
 
-	// Convert milliseconds to microseconds
-	int microseconds = milliseconds * 1000;
+    // Convert 'milliseconds' into seconds and microseconds
+    int32_t seconds = milliseconds / 1000;
+    int32_t microseconds = (milliseconds - seconds * 1000) * 1000;
+    
+    // We're going to wait the specified number of milliseconds for the event
+    // to become triggered
+    timeval timeout = {seconds, microseconds};
 
-	// We're going to wait the specified number of milliseconds for the event
-	// to become triggered
-	timeval timeout = {microseconds / 1000000, microseconds % 1000000};
+    // Decide whether to pass "select" a timeval or a NULL
+    timeval* p_timeout = (milliseconds == 0) ? nullptr : &timeout;
 
-	// We're only polling just the one specified file-descriptor
-	FD_ZERO(&rfds);
-	FD_SET(m_fd, &rfds);
+    // We're only polling just the one specified file-descriptor
+    FD_ZERO(&rfds);
+    FD_SET(m_fd, &rfds);
 
-	// Is there data on this descriptor?
-	int count = select(m_fd+1, &rfds, NULL, NULL, &timeout);
+    // Is there data on this descriptor?
+    int count = select(m_fd+1, &rfds, NULL, NULL, p_timeout);
 
-	// If there is no data available, tell the caller that we're untriggered
-	if (count < 1) return 0;
+    // If there is no data available, tell the caller that we're untriggered
+    if (count < 1) return 0;
 
-	// We're triggered.  Read the event value
-	dummy_size_t = read(m_fd, &event_value, sizeof(event_value));
+    // We're triggered.  Read the event value
+    read(m_fd, &event_value, sizeof(event_value));
 
-	// Hand the caller the event value
-	return event_value;
+    // Hand the caller the event value
+    return event_value;
 }
 //============================================================================
--- cpp_framework/event.h
+++ cpp_framework/event.h
@@ -12,35 +12,35 @@ class CEvent
 {
 public:
 
-	// Constructs an event object in the "untriggered" state
-	CEvent();
+    // Constructs an event object in the "untriggered" state
+    CEvent();
 
-	// Closes the event file-descriptor
-	~CEvent();
+    // Closes the event file-descriptor
+    ~CEvent();
 
-	// Resets the event to the "untriggered" state
-	void	Reset();
+    // Resets the event to the "untriggered" state
+    void    reset();
 
-	// Triggers the event with the specified value.  If the event is already
-	// triggered, this will be added to the existing event value
-	void	Set(U64 value = 1);
+    // Triggers the event with the specified value.  If the event is already
+    // triggered, this will be added to the existing event value
+    void    set(uint64_t value = 1);
 
-	// Wait "milliseconds" for the event to become triggered.  Returns either
-	// the triggered value, or zero if the event hasn't happened yet.  If the
-	// return value is non-zero, the event is automatically reset to the
-	// "untriggered" state
- 	U64 	Get(int milliseconds = 0);
+    // Wait "milliseconds" for the event to become triggered.  Returns either
+    // the triggered value, or zero if the event hasn't happened yet.  If the
+    // return value is non-zero, the event is automatically reset to the
+    // "untriggered" state.  If milliseconds is 0, it will wait forever
+    uint64_t wait(uint32_t milliseconds = 0);
 
-	// Returns 'true' if the event is in the "triggered" state
-	bool	IsTriggered();
+    // Returns 'true' if the event is in the "triggered" state
+    bool    is_triggered();
 
-	// Returns the event file descriptor for use with "select"
-	int		FD() {return m_fd;}
+    // Returns the event file descriptor for use with "select"
+    int     fd() {return m_fd;}
 
 
 protected:
 
-	int 	m_fd;
+    int     m_fd;
 
 };
 //============================================================================
