eb23b34d91be252438b2e4f0ddb882443c22d695
DouglasWWolf <douglaswolf@yahoo.com>
2022-06-21 21:00:05 -0700

    See History.h for changes

--- main.cpp
+++ main.cpp
@@ -3,30 +3,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "cthread.h"
 #include "config_file.h"
-#include "netsock.h"
 
 
 using namespace std;
 
 void test_config();
 
-NetSock sock;
 
 int main()
 {
-    if (!sock.create_server(12345))
-    {
-        printf(">>>>> CREATE FAILED <<<<<<\n");
-        exit(1);
-    }
-
-    sock.listen_and_accept();
-    string pa = sock.get_peer_address();
-    printf("The client IP is %s\n", pa.c_str());
-
-
     test_config();
     return 0;
 }
